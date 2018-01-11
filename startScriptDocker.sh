49c329508836d6ec609dcbf652b867e053a946ff
Anshul Jindal <anshuljindal.nith@gmail.com>
2018-01-11 16:45:48 +0100

    Update startScriptDocker.sh

--- startScriptDocker.sh
+++ startScriptDocker.sh
@@ -19,7 +19,7 @@ sudo apt-get update
 #docker Installation
 sudo apt-get install -y docker-ce
 sudo curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 #nodejs Installation
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 sudo apt-get install -y nodejs
