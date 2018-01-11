e32250b1c4dd14977137b565ecdc40e1e8316000
Anshul Jindal <anshuljindal.nith@gmail.com>
2018-01-11 16:38:14 +0100

    Update runScript.sh

--- runScript.sh
+++ runScript.sh
@@ -18,7 +18,7 @@ sudo add-apt-repository \
 sudo apt-get update
 #docker Installation
 sudo apt-get install -y docker-ce
-sudo curl -L https://github.com/docker/compose/releases/download/1.11.2/docker-compose-`uname -s`-`uname -m` > /usr/bin/docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/bin/docker-compose
 #nodejs Installation
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
